3958356e7b8af6371fc41a0a87f064b756720422
heksesang <heksesang@gmail.com>
2012-01-31 10:29:07 +0100

    Updates for non-posix support.

--- sabtoanidb.py
+++ sabtoanidb.py
@@ -11,7 +11,10 @@ if len(sys.argv) < 2:
 	print "No folder supplied - is this being called from SABnzbd?"
 	sys.exit()
 else:
-	command= os.path.join(os.path.dirname(sys.argv[0]), "anidb.py") +" -r -n -m -a -w -x '" + sys.argv[1]+"'"
-	args = shlex.split(command)
+	command= "python " + os.path.join(os.path.dirname(sys.argv[0]), "anidb.py") + " -r -n -m -a -w -x '" + sys.argv[1] + "'"
+	if (os.name != "posix"):
+		args = shlex.split(command, posix=False)
+	else
+		args = shlex.split(command)
 	retcode = subprocess.call(args)
 	sys.exit(retcode)
