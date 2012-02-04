3a4468f403bc2b55ada4327d5060fadd297b02f4
heksesang <heksesang@gmail.com>
2012-02-04 12:40:51 +0100

    Removed the explicit call to python,

--- sabtoanidb.py
+++ sabtoanidb.py
@@ -14,7 +14,7 @@ if len(sys.argv) < 2:
 	print "No folder supplied - is this being called from SABnzbd?"
 	sys.exit()
 else:
-	command = "python " + os.path.join(os.path.dirname(sys.argv[0]), "anidb.py") + " -r -n -m -a -w -x '" + sys.argv[1] + "'"
+	command = os.path.join(os.path.dirname(sys.argv[0]), "anidb.py") + " -r -n -m -a -w -x '" + sys.argv[1] + "'"
 	if (os.name != "posix"):
 		args = shlex.split(command, posix=False)
 	else
