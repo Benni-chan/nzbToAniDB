270563777ad085b05844b3cde0ab703f7d3b3f20
Benni-chan <benjamin@mycontact.eu>
2012-10-13 23:40:10 +0200

    fix for sickbeard integration (revert earlier commit)
    sabtosickbeard_script must be set to the sabtosickbeard script name.
    as far as i can tell, this is still broken, since currently sabtoanidb
    is called again (recursive loop).
    it would need to call the sabtosickbeard script maybe...

--- sabToAniDB.py
+++ sabToAniDB.py
@@ -32,7 +32,7 @@ else:
 	retcode = subprocess.call(args)
 	
 	# For SickBeard integration
-	if (os.path.exists(os.path.join(os.path.dirname(sys.argv[0]) and config['sabtosickbeard_script'])) == True):
+	if (os.path.exists(os.path.join(os.path.dirname(sys.argv[0]), config['sabtosickbeard_script'])) == True):
 		subprocess.call(sys.argv)
 	
 	sys.exit(retcode)
